1cd80efb6f580830a00946757232bc80c86087e6
LuBieS2 <119162617+LuBieS2@users.noreply.github.com>
2024-10-20 13:03:34 +0200

    Pong

---
+++ main.py
@@ -0,0 +1,85 @@
+import pygame
+import random
+#screen resolution and basic commands
+pygame.display.set_caption('Pong')
+pygame.init()
+X=1280
+Y=720
+screen = pygame.display.set_mode((X, Y))
+clock = pygame.time.Clock()
+running = True
+dt=0
+start=False
+#default position of objects
+player1_pos = pygame.Vector2(screen.get_width() / 10, screen.get_height() / 2)
+player2_pos = pygame.Vector2(screen.get_width() - screen.get_width()/10, screen.get_height() / 2)
+ball = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+#ball speed and direction
+speed=300
+abx = random.uniform(0, speed)
+aby = speed-abx
+s=random.randint(0, 3)
+score1=0
+score2=0
+if s==0:
+    bx=abx
+    by=aby
+if s==1:
+    bx=-abx
+    by=aby
+if s==2:
+    bx=abx
+    by=-aby
+if s==3:
+    bx=-abx
+    by=-aby
+#playerscore
+score1, score2 = 0, 0
+
+while running:
+    screen.fill((0, 0, 0))
+    #drawing player score
+
+    #closing game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #drawing score
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    scorep1display = font.render(str(score1), True, (255, 255, 255))
+    p1rect = scorep1display.get_rect()
+    p1rect.center = (X // 4, Y//8)
+    screen.blit(scorep1display, p1rect)
+    scorep2display = font.render(str(score2), True, (255, 255, 255))
+    p2rect = scorep2display.get_rect()
+    p2rect.center = (X-X // 4, Y//8)
+    screen.blit(scorep2display, p2rect)
+    #drawing objects
+    pygame.draw.rect(screen, "white", (player1_pos.x, player1_pos.y, 20, 150), 0)
+    pygame.draw.rect(screen, "white", (player2_pos.x, player2_pos.y, 20, 150), 0)
+    pygame.draw.lines(screen, "white", False, [(screen.get_width() / 2, screen.get_height()), (screen.get_width() / 2, 0)], 2)
+    pygame.draw.circle(screen, "white", (ball.x, ball.y), 10, 0)
+    dt = clock.tick(60) / 1000
+    #controls
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player1_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player1_pos.y += 300 * dt
+    if keys[pygame.K_UP]:
+        player2_pos.y += 300 * dt
+    if keys[pygame.K_DOWN]:
+        player2_pos.y -= 300 * dt
+    if keys[pygame.K_SPACE]:
+        start=True
+    #ball physics
+    if ball.y>=screen.get_height() or ball.y<=0:
+        by=-by
+    if player1_pos.y <= ball.y <= player1_pos.y+150 and player1_pos.x <= ball.x <= player1_pos.x+20:
+        bx=-bx
+    if player2_pos.y <= ball.y <= player2_pos.y+150 and player2_pos.x <= ball.x <= player2_pos.x+20:
+        bx=-bx
+    if start:
+        ball.x += bx*dt
+        ball.y += by*dt
+    pygame.display.flip()
