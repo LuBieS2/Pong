e46141d05cb20204fc8a8a8e6b6bca537660ad47
LuBieS2 <119162617+LuBieS2@users.noreply.github.com>
2024-10-20 18:41:42 +0200

    pong

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 import random
+xx=0
+yy=0
 #screen resolution and basic commands
+score1=0
+score2=0
 pygame.display.set_caption('Pong')
 pygame.init()
 X=1280
@@ -15,27 +19,29 @@ player1_pos = pygame.Vector2(screen.get_width() / 10, screen.get_height() / 2)
 player2_pos = pygame.Vector2(screen.get_width() - screen.get_width()/10, screen.get_height() / 2)
 ball = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 #ball speed and direction
-speed=700
-abx = random.uniform(0, speed)
-aby = speed-abx
-s=random.randint(0, 3)
-score1=0
-score2=0
-if s==0:
-    bx=abx
-    by=aby
-if s==1:
-    bx=-abx
-    by=aby
-if s==2:
-    bx=abx
-    by=-aby
-if s==3:
-    bx=-abx
-    by=-aby
+def ball_random_direction():
+    speed=700
+    abx = random.uniform(speed/2, speed*3/4)
+    aby = (speed**2-abx**2)**(1/2)
+    s=random.randint(0, 3)
+    if s==0:
+        bx=abx
+        by=aby
+    if s==1:
+        bx=-abx
+        by=aby
+    if s==2:
+        bx=abx
+        by=-aby
+    if s==3:
+        bx=-abx
+        by=-aby
+    print(bx,by)
+    return bx,by
+b=ball_random_direction()
+bx, by = b[0], b[1]
 #playerscore
 score1, score2 = 0, 0
-
 while running:
     screen.fill((0, 0, 0))
     #closing game
@@ -63,13 +69,18 @@ while running:
     #controls
     pspeed=500
     keys = pygame.key.get_pressed()
+    motion=[False, False,False,False]
     if keys[pygame.K_w]:
         player1_pos.y -= pspeed * dt
+        motion[0]=True
     if keys[pygame.K_s]:
+        motion[1]=True
         player1_pos.y += pspeed * dt
     if keys[pygame.K_UP]:
+        motion[2]=True
         player2_pos.y -= pspeed * dt
     if keys[pygame.K_DOWN]:
+        motion[3]=True
         player2_pos.y += pspeed * dt
     if keys[pygame.K_SPACE]:
         start=True
@@ -87,8 +98,16 @@ while running:
         score2+=1
         ball.x=X//2
         ball.y=Y//2
+        b = ball_random_direction()
+        bx, by = b[0], b[1]
     if ball.x>=X:
         score1+=1
         ball.x=X//2
         ball.y=Y//2
+        b = ball_random_direction()
+        bx, by = b[0], b[1]
+
+    print(ball.distance_to((xx, yy)))
+    xx=ball.x + bx*dt
+    yy=ball.y + by*dt
     pygame.display.flip()
